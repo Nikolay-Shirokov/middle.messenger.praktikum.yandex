48c94b3e44c0563ac3c042f6b660736704a6a540
Nick Shirokov <nick_4312@mail.ru>
2022-05-02 12:23:52 +0300

    feat: Сделан роутинг

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import './index.css';
 import renderSignUp from "./pages/signup";
+import renderSignIn from "./pages/signin";
 
 const root = document.querySelector('#root');
 
-root.innerHTML = renderSignUp();
+function renderPage() {
+
+  const pathname = window.location.pathname;
+
+  const routes = {
+    '/signup': renderSignUp,
+    '/signin': renderSignIn,
+  }
+
+  const currentRoute = routes[pathname];
+/*   if (!currentRoute) {
+    return renderErrorPageNotFound();
+  } */
+
+  return currentRoute();
+
+}
+
+root.innerHTML = renderPage();
